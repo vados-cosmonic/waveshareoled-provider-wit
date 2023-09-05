1f219816f363bded514d78b8082313cd1f25b456
Victor Adossi <vadossi@cosmonic.com>
2023-09-05 18:50:38 -0400

    chore: initial commit
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use async_trait::async_trait;
+
+use wasmcloud_core::LinkDefinition;
+use wasmcloud_provider_sdk::{Context, error::ProviderResult};
+
+wasmcloud_provider_wit_bindgen::generate!(
+    WaveshareOledProvider,
+    "cosmonic:waveshareoled",
+    "waveshareoled"
+);
+
+/// Implementation struct for Waveshare OLED provider
+struct WaveshareOledProvider;
+
+#[async_trait]
+impl WasmcloudCapabilityProvider for WaveshareOledProvider {
+    async fn put_link(&self, ld: &LinkDefinition) -> bool {
+        false // TODO
+    }
+
+    async fn delete_link(&self, actor_id: &str) {
+        panic!("not implemented"); // TODO
+    }
+
+    async fn shutdown(&self) {
+        panic!("not implemented") // TODO
+    }
+}
+
+#[async_trait]
+impl CosmonicWaveshareoledScreen for WaveshareOledProvider {
+    async fn draw_message(&self, ctx: Context, msg: String) -> ProviderResult<Result<(), String>> {
+        panic!("NOPE");
+    }
+}
